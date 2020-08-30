91fb27eed829e32e4ad26f45c4d074fd05d74555
Samuel Simmons <sjsimmo123@gmail.com>
2020-08-30 18:31:09 +1000

    Improved player physics. Added to world map.

--- Platformer/src/player.cpp
+++ Platformer/src/player.cpp
@@ -14,40 +14,136 @@ void player :: init(double _x_pos, double _y_pos)
 
 //called every physics frame
 void player :: update(double dt, const map &Map, const events &Events)
-{
+{   
     //walk
-    if(Events.left_held)
+    if(wall_collisions&0b0010)
     {
-        x_vel = -120;
-        texture = 2;
+        if(Events.left_held)
+        {
+            x_acc = -walk_acc;
+            texture = 2;
+            if(x_vel<-walk_vel-x_acc*dt)
+            {
+                x_vel = -walk_vel;
+                x_acc = 0;
+            }
+        }
+        else if(Events.right_held)
+        {
+            x_acc = walk_acc;
+            texture = 3;
+            if(x_vel>walk_vel-x_acc*dt)
+            {
+                x_vel = walk_vel;
+                x_acc = 0;
+            }
+        }
+        else
+        {
+            if(x_vel<-walk_acc*dt)
+            {
+                x_acc = walk_acc;
+            }
+            else if(x_vel>walk_acc*dt)
+            {
+                x_acc = -walk_acc;
+            }
+            else
+            {
+                x_vel = 0;
+                x_acc = 0;
+                texture = 1;
+            }    
+        }
     }
-    else if(Events.right_held)
-    {
-        x_vel = 120;
-        texture = 3;
-    }
+    //airwalk
     else
     {
-        x_vel = 0;
-        texture = 1;
+        if(Events.left_held)
+        {
+            x_acc = -air_acc;
+            texture = 2;
+            if(x_vel<-walk_vel-x_acc*dt)
+            {
+                x_vel = -walk_vel;
+                x_acc = 0;
+            }
+        }
+        else if(Events.right_held)
+        {
+            x_acc = air_acc;
+            texture = 3;
+            if(x_vel>walk_vel-x_acc*dt)
+            {
+                x_vel = walk_vel;
+                x_acc = 0;
+            }
+        }
+        else
+        {
+            if(x_vel<-air_acc*dt)
+            {
+                x_acc = air_acc;
+            }
+            else if(x_vel>air_acc*dt)
+            {
+                x_acc = -air_acc;
+            }
+            else
+            {
+                x_vel = 0;
+                x_acc = 0;
+                texture = 1;
+            }    
+        }
     }
+    
     //jump
     if(wall_collisions&0b0010)
     {
         airjumps_left = airjumps;
+        jump_was_pressed = false;
     }
     if(Events.jump_pressed && (wall_collisions&0b0010))
     {
-        y_vel = -200;  
+        y_vel = -jump_vel;
+        jump_was_pressed = true;
     }
-    else if(Events.jump_pressed && airjumps_left>0)
+    //walljump
+    else if(Events.jump_pressed && (wall_collisions&0b1000))
     {
+        y_vel = -walljump_vel;
+        x_vel = walljump_x_vel;
+        jump_was_pressed = true;
+    }
+    else if(Events.jump_pressed && (wall_collisions&0b0001))
+    {
+        y_vel = -walljump_vel;
+        x_vel = -walljump_x_vel;
+        jump_was_pressed = true;
+    }
+    //airjump
+    else if(Events.jump_pressed && airjumps_left>0)
+    { 
+        y_vel = -airjump_vel;
         airjumps_left -= 1;
-        y_vel = -200;
+        jump_was_pressed = true;
     }
     //gravity
-    y_acc = 400;
-    
+    if(!Events.jump_held && jump_was_pressed && y_vel<-50)
+    {
+        y_acc = endjump_acc;
+    }
+    else
+    {
+        y_acc = gravity;
+    }   
+    //wallslide
+    if(y_vel>wallslide_vel-y_acc*dt && (wall_collisions&0b1000 || wall_collisions&0b0001))
+    {
+        y_vel = wallslide_vel-y_acc*dt;
+    }
+
     //move entity
     entity_move(dt);
     wall_collisions = entity_wall_collisions(Map, true);
--- Platformer/src/player.h
+++ Platformer/src/player.h
@@ -6,9 +6,22 @@
 class player: public entity
 {
 private:
+    //Physics Tuning Parameters
+    double walk_acc = 800;
+    double walk_vel = 150;
+    double air_acc = 250;
+    double jump_vel = 180;
+    int airjumps = 1;
+    double airjump_vel = 150;
+    double walljump_vel = 160;
+    double walljump_x_vel = 150;
+    double endjump_acc = 1300;
+    double wallslide_vel = 100;
+    double gravity = 300;
+
     char wall_collisions = 0;
     int airjumps_left = 0;
-    int airjumps = 1;
+    int jump_was_pressed = false;
 public:
     void init(double _x_pos, double _y_pos);
     void update(double dt, const map &Map, const events &Events);
